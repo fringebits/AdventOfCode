cf3af4e9bf6ef019882a1a128b73e6c2b0f7b35b
MikeG <mikeg@fringebits.com>
2021-12-01 22:01:18 -0800

    Day 2: Dive! (and fixed issue with day1 import statement)

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -1,31 +1,79 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import utils
 
-sample_input = []
+sample_input = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2"]
 
 @utils.timer
 def run():
     print("\n***** Day 2 *****")
     
-    # f = open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r')
-    # input = list(map(int, f.readlines()))
-    # f.close()
+    f = open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r')
+    input = [line.strip() for line in f]
+    f.close()
 
     # part 1:
     #run_part1(sample_input)
-    #run_part1(input)
+    run_part1(input)
 
     # part 2:
     #run_part2(sample_input)
-    #run_part2(input)
+    run_part2(input)
 
 def run_part1(input):
-    "part1"
+    "part1: Dive"
+    # forward X increases the horizontal position by X units.
+    # down X increases the depth by X units.
+    # up X decreases the depth by X units.
+
+    pos = utils.Vec2(0, 0)
+
+    for line in input:
+        delta = parse_move(line)
+        pos += delta
+    print("part1: {0}, result = {1}".format(pos, pos.X * pos.Y))
+    
 
 def run_part2(input):
-    "part2"
+    "part2: Dive and Aim"
+    # down X increases your aim by X units.
+    # up X decreases your aim by X units.
+    # forward X does two things:
+    #   It increases your horizontal position by X units.
+    #   It increases your depth by your aim multiplied by X.    
+    aim = 0
+    pos = utils.Vec2(0, 0)
+    for line in input:
+        delta = parse_move(line)
+        aim += delta.Y
+        pos.X += delta.X
+        pos.Y += aim * delta.X
+    print("part2: {0}, result = {1}".format(pos, pos.X * pos.Y))
+
+def parse_move(line):
+    "Parse single line, return Vec2 representing the move"
+    # Parse single line, using a "Forward-Down axis"
+    # forward X:  (X, 0)
+    # up X: (0, -X)
+    # down X: (0, X)
+    pair = line.split(' ')
+    result = utils.Vec2(0, 0)
+
+    if (pair[0]== 'up'):
+        result.Y = -int(pair[1])
+    elif (pair[0]== 'down'):
+        result.Y = int(pair[1])
+    elif (pair[0]== 'forward'):
+        result.X = int(pair[1])
+
+    return result
 
 if __name__ == "__main__":
     run()
