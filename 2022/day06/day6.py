e19d0278abff0fd0bfbc97cf1251c7b10e04a1c3
mikeg <mikeg@fringebits.com>
2022-12-06 08:08:29 -0800

    Day 6, Tuning Trouble

---
+++ 2022/day06/day6.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import utils
+
+class Puzzle(utils.PuzzleBase):
+    def __init__(self):
+        super().__init__(6, "Tuning Trouble", os.path.dirname(__file__))
+        self.answers = [1140, None]
+
+    def test_marker(line, count):
+        for ii in range(0, len(line)-count):
+            marker = set(line[ii:ii+count])
+            if len(marker) == count:
+                print(f"marker={marker}, ii={ii+count}")
+                return ii+count
+
+    def run_part1(self):
+        return Puzzle.test_marker(self.input[0], 4)
+
+    def run_part2(self):
+        return Puzzle.test_marker(self.input[0], 14)
+
+@utils.timer
+def run():
+    Puzzle().run()
+
+if __name__ == "__main__":
+    run()
